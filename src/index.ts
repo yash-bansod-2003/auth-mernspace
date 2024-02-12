20651f1700bce45d057405f886ec2226222ca84a
yash bansod <yashbansodgit@gmail.com>
2024-02-12 15:14:39 +0530

    chore: check for lintstagedrc working

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ const port = CONFIG.PORT || 5001;
 const server = createServer();
 
 server.listen(port, () => {
-    try {
-        logger.info(`Listening on port ${port}`);
-    } catch (error) {
-        logger.error(`Error ${(error as Error).message}`);
-        process.exit(1);
-    }
+  try {
+    logger.info(`Server Listening on port ${port}`);
+  } catch (error) {
+    logger.error(`Error ${(error as Error).message}`);
+    process.exit(1);
+  }
 });
