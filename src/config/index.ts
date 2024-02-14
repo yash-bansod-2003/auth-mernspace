1cd266c1257bcd3a49daca7879a08625a9af389f
yash bansod <yashbansodgit@gmail.com>
2024-02-14 14:23:32 +0530

    refactor: relocate envs to environment folder

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,10 @@ import path from "node:path";
 import { config } from "dotenv";
 
 config({
-  path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}.local`),
+  path: path.join(
+    __dirname,
+    `../../environments/.env.${process.env.NODE_ENV}.local`,
+  ),
 });
 
 const { PORT, NODE_ENV } = process.env;
