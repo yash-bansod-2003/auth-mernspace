78dd4309d05dcc3cddbfa4b61b6849847cf9dbc5
yash bansod <yashbansodgit@gmail.com>
2024-02-14 14:34:35 +0530

    refactore: change tests writing style

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -2,21 +2,25 @@ import supertest from "supertest";
 import { createServer } from "../server";
 
 describe("server", () => {
-  it("health check should returns 200", async () => {
-    await supertest(createServer())
-      .get("/status")
-      .expect(200)
-      .then((res) => {
-        expect(res.ok).toBe(true);
-      });
+  describe("get /status", () => {
+    it("should returns status 200", async () => {
+      await supertest(createServer())
+        .get("/status")
+        .expect(200)
+        .then((res) => {
+          expect(res.ok).toBe(true);
+        });
+    });
   });
 
-  it("message endpoint should says hello", async () => {
-    await supertest(createServer())
-      .get("/message/john")
-      .expect(200)
-      .then((res) => {
-        expect(res.body).toEqual({ message: "hello john" });
-      });
+  describe("get /message/:name", () => {
+    it("should endpoint says hello", async () => {
+      await supertest(createServer())
+        .get("/message/john")
+        .expect(200)
+        .then((res) => {
+          expect(res.body).toEqual({ message: "hello john" });
+        });
+    });
   });
 });
