ea666b0bcfc26e0019d3abc1cea2da6fb711c97c
yash bansod <yashbansodgit@gmail.com>
2024-03-16 18:30:23 +0530

    feat: file transports for logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -8,6 +8,18 @@ const logger = createLogger({
       level: "info",
       format: format.combine(format.timestamp(), format.simple()),
     }),
+    new transports.File({
+      dirname: "logs",
+      filename: "combined.log",
+      level: "info",
+      format: format.combine(format.timestamp(), format.simple()),
+    }),
+    new transports.File({
+      dirname: "logs",
+      filename: "errors.log",
+      level: "error",
+      format: format.combine(format.timestamp(), format.simple()),
+    }),
   ],
 });
 
