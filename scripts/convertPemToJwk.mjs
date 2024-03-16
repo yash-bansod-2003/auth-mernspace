83c09d02c49d9b906db87bee9054683ab11db940
yash bansod <yashbansodgit@gmail.com>
2024-03-17 00:35:59 +0530

    feat: srcript pem to jwk format

--- scripts/convertPemToJwk.mjs
+++ scripts/convertPemToJwk.mjs
@@ -1,10 +1,14 @@
 import fs from "node:fs";
-import rsaPemToJwk from "rsa-pem-to-jwk"
+import rsaPemToJwk from "rsa-pem-to-jwk";
 
 const privateKey = fs.readFileSync("./certs/private.pem");
 
-const jwk = rsaPemToJwk(privateKey, {
-      use: "sig"
-}, "public");
+const jwk = rsaPemToJwk(
+  privateKey,
+  {
+    use: "sig",
+  },
+  "public",
+);
 
 console.log(JSON.stringify(jwk));
