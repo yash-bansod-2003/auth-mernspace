a04a1abb382bd5db78509f0648ce63a9c552dafc
yash bansod <yashbansodgit@gmail.com>
2024-03-17 14:23:29 +0530

    refactor: logger debugging

--- src/controllers/tenant.controller.ts
+++ src/controllers/tenant.controller.ts
@@ -2,6 +2,7 @@ import { TenantService } from "@/services/tenant.service";
 import { AuthenticatedRequest, TenantData } from "@/types";
 import { NextFunction, Response } from "express";
 import { validationResult } from "express-validator";
+import { Logger } from "winston";
 
 interface TenantCreateRequest extends AuthenticatedRequest {
   body: TenantData;
@@ -9,12 +10,16 @@ interface TenantCreateRequest extends AuthenticatedRequest {
 
 class TenantController {
   private tenantService: TenantService;
+  private logger: Logger;
 
-  constructor(tenantService: TenantService) {
+  constructor(tenantService: TenantService, logger: Logger) {
     this.tenantService = tenantService;
+    this.logger = logger;
   }
 
   async create(req: TenantCreateRequest, res: Response, next: NextFunction) {
+    this.logger.debug("New Request to create tenant");
+
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -24,7 +29,9 @@ class TenantController {
     const { name, address } = req.body;
 
     try {
+      this.logger.info("creating new tenant..!");
       const tenant = await this.tenantService.create({ name, address });
+      this.logger.info("created new tenant..!");
       return res.status(201).json(tenant);
     } catch (error) {
       return next(error);
--- src/routes/tenant.router.ts
+++ src/routes/tenant.router.ts
@@ -4,14 +4,17 @@ import { AppDataSource } from "@/data-source";
 import { Tenant } from "@/entity/tenant";
 import { TenantService } from "@/services/tenant.service";
 import { AuthenticatedRequest } from "@/types";
+import { tenentCreateValidator } from "@/lib/validators/tenant";
+import { logger } from "@/config/logger";
 
 const router = express.Router();
 const tenantRepository = AppDataSource.getRepository(Tenant);
 const tenantService = new TenantService(tenantRepository);
-const tenantController = new TenantController(tenantService);
+const tenantController = new TenantController(tenantService, logger);
 
 router.post(
   "/tenant",
+  tenentCreateValidator,
   async (req: Request, res: Response, next: NextFunction) => {
     await tenantController.create(req as AuthenticatedRequest, res, next);
   },
