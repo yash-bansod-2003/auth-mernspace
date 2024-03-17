c4281aa861829139d74d7d0a4dd1d277241fe8d8
yash bansod <yashbansodgit@gmail.com>
2024-03-17 14:09:10 +0530

    test: save tenent in database

--- src/__tests__/tenant/create.test.ts
+++ src/__tests__/tenant/create.test.ts
@@ -3,6 +3,7 @@ import { createServer } from "@/server";
 import supertest from "supertest";
 import { DataSource } from "typeorm";
 import createJWKSMock from "mock-jwks";
+import { Tenant } from "@/entity/tenant";
 
 describe("tenent create", () => {
   let connection: DataSource;
@@ -29,12 +30,37 @@ describe("tenent create", () => {
 
   describe("post /api/tenant", () => {
     it("should return status 201", async () => {
+      const tenantData = {
+        name: "Tenant Name",
+        address: "Tenant Address",
+      };
+
       await supertest(createServer())
         .post("/api/tenant")
+        .send(tenantData)
         .expect(201)
         .then((res) => {
           expect(res.ok).toBe(true);
         });
+    });
+
+    it("should save tenant in database", async () => {
+      const tenantData = {
+        name: "Tenant Name",
+        address: "Tenant Address",
+      };
+
+      await supertest(createServer())
+        .post("/api/tenant")
+        .send(tenantData)
+        .expect(201);
+
+      const tenantRepository = connection.getRepository(Tenant);
+      const tenants = await tenantRepository.find();
+
+      expect(tenants).toHaveLength(1);
+      expect(tenants[0].name).toBe(tenantData.name);
+      expect(tenants[0].address).toBe(tenantData.address);
     });
   });
 });
