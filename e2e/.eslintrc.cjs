017304ddcf8d587d1bc1d3a8cec5ef0e8ae62b0b
= <yashbansodgit@gmail.com>
2024-06-29 15:49:08 +0530

    chore: remove e2e testing

--- e2e/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.*", "jest.config.*"],
-};
