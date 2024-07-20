7b3e27b856242f7186f943232e17f658bbf77e84
= <yashbansodgit@gmail.com>
2024-07-20 11:16:04 +0530

    feat: swagger setup

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,31 @@
+import path from "node:path";
+import fs from "node:fs";
 import express, { type Express, urlencoded, json } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yaml";
 import { errorHandler } from "@/middlewares/error-handler";
 import { authRouter } from "@/routes/auth.router";
 import { tenantRouter } from "@/routes/tenant.router";
 import { userRouter } from "@/routes/user.router";
 
+const swaggerFile = fs.readFileSync(
+  path.join(__dirname, "../public/swagger.yaml"),
+  "utf8",
+);
+
+const swaggerDocument = YAML.parse(swaggerFile) as swaggerUi.JsonObject;
+
 export const createServer = (): Express => {
   const app = express();
   app
+    .use(
+      "/api-docs",
+      swaggerUi.serve,
+      swaggerUi.setup(swaggerDocument, { explorer: true }),
+    )
     .use(cors({ origin: ["http://localhost:5173"], credentials: true }))
     .use(json())
     .use(urlencoded({ extended: true }))
